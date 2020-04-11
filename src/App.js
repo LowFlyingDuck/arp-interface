a24c254528b7171d68c505cc770ee09377b42a08
data4040 <51282810+data4040@users.noreply.github.com>
2020-04-11 23:21:30 +0200

    first release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  var victims = [];
+  var [status, setStatus] = useState({
+    value: 'connecting...',
+    src: '/icons/negative.svg'
+  });
+  var [arping, setArping] = useState({
+    value: 'not arping',
+    src: '/icons/negative.svg'
+  });
+  function update() {
+    if (navigator.onLine) {
+      setStatus({
+        value: 'online',
+        src: '/icons/positive.svg'
+      })
+    } else {
+      setStatus({
+        value: 'offline',
+        src: '/icons/negative.svg'
+      })
+    }
+    let active = false;
+    for (let victim of victims) {
+      if (victim.active) {
+        setArping({
+          value: 'arping',
+          src: '/icons/positive.svg'
+        })
+        active = true;
+        break;
+      }
+    }
+    if (active === false) setArping({
+      value: 'not arping',
+      src: '/icons/negative.svg'
+    })
+  }
+  setInterval(update, 2000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <nav>
+        <h1><span>Arp </span>Interface</h1>
+        <section>
+          <div>
+            <img src={status.src} alt="status" />
+            <h2>{status.value}</h2>
+          </div>
+          <div>
+            <img src={arping.src} alt="arping" />
+            <h2>{arping.value}</h2>
+          </div>
+        </section>
+      </nav>
+      <main>
+        <h1>Saved Victims</h1>
+        <div className="loading">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        {}
+      </main>
+    </React.StrictMode>
   );
 }
 
---
+++ src/components/Victim.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default function Victim() {
+  return (
+    <section>
+      <img src="/icons/switch.svg" alt="off" />
+    </section>
+  )
+}
